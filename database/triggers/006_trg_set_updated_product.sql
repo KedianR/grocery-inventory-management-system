70e4e1c160b556016745737a67660d51276eeea1
k_d <kedianronz5ultra@gmail.com>
2026-07-09 02:07:49 +0530

    added the final trigger for updating the timestamp whenever a change is made to any product

---
+++ database/triggers/006_trg_set_updated_product.sql
@@ -0,0 +1,17 @@
+create or replace function set_updated_product()
+returns trigger as $$ 
+begin
+	if new.product_id <> old.product_id then 
+	raise exception 'Changing product ids not allowed manually delete and reinsert please !!!';
+	end if;
+	update  product 
+	set updated_at = CURRENT_TIMESTAMP
+	where product_id = new.product_id;
+return new;
+end;
+$$ language plpgsql
+
+create trigger trg_set_updated_product
+before update 
+on product
+for each row execute function  set_updated_product();
